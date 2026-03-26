32ca0a27116d5d1d8a255d7ebbe5fc52e63de804
Rockylane01 <rockylane01@gmail.com>
2026-03-25 22:53:17 -0600

    adjust organization of frontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
-import { useState } from 'react';
 import './App.css'
-import BookList from './components/BookList'
-import CategoryFilter from './components/CategoryFilter'
+import BooksPage from './pages/BooksPage'
 
 function App() {
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-
   return (
     <>
-      <div className="container">
-        <h1>Book Store</h1>
-        <div className="row">
-          <div className="col-md-3">
-            <CategoryFilter 
-              selectedCategories={selectedCategories} 
-              setSelectedCategories={setSelectedCategories} 
-            />
-          </div>
-          <div className="col-md-9">
-            <BookList selectedCategories={selectedCategories} />
-          </div>
-        </div>
-      </div>
+      <BooksPage />
     </>
   )
 }
